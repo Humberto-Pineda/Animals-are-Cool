10109d12ad187c3e1dd8da9b5777ed1630ccb2c9
Humberto Pineda <bpineda7540@gmail.com>
2022-07-08 02:17:22 -0700

    javascript language

---
+++ app.js
@@ -0,0 +1,7 @@
+let firstName = prompt("What type of bear is best?");
+console.log (firstName);
+if (firstName == "black bear"){document.write("...It's true")}
+else {
+    document.write("Wrong! Black bear.");
+    }
+    
