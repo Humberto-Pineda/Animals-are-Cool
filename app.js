676789cab4ff081df2558b960586692ab224b8b3
Humberto Pineda <bpineda7540@gmail.com>
2022-07-08 17:40:35 -0700

    added more prompts

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
-let firstName = prompt("What type of bear is best?");
-console.log (firstName);
-if (firstName == "black bear"){document.write("...It's true")}
-else {
-    document.write("Wrong! Black bear.");
+function bestBear() {
+    let favBear = prompt("What type of bear is best?");
+    console.log (favBear);
+    if (favBear == "black bear"){
+        return "...It's true"
     }
-    
+    else {
+        return "Wrong! Black bear."
+    }
+}
+
+function bestLand() {
+    let favAnimal = prompt("What is your favorite land animal?");
+    if (favAnimal == "bear"){
+        return "That's mine too!"
+    }
+    else {
+        return "That's a great choice!"
+    }
+}
+
+function bestSea() {
+    let favAnimal = prompt("What is your favorite sea animal?");
+    if (favAnimal == "orca"){
+        return "That's mine too!"
+    }
+    else {
+        return "That's a great choice!"
+    }
+}
+
+function bestSky() {
+    let favAnimal = prompt("What is your favorite flying animal?");
+    if (favAnimal == "eagle"){
+        return "That's mine too!"
+    }
+    else {
+        return "That's a great choice!"
+    }
+}
+
