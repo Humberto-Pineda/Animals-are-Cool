00e98fc6045affa098445de4594b69db34c81c24
Humberto Pineda <bpineda7540@gmail.com>
2022-07-09 14:49:45 -0700

    took out 3 prompts

--- app.js
+++ app.js
@@ -8,36 +8,6 @@ function bestBear() {
     }
 }
 
-function bestLand() {
-    let favAnimal = prompt("What is your favorite land animal?");
-    if (favAnimal == "bear"){
-        return "That's mine too!"
-    }
-    else {
-        return "That's a great choice!"
-    }
-}
-
-function bestSea() {
-    let favAnimal = prompt("What is your favorite sea animal?");
-    if (favAnimal == "orca"){
-        return "That's mine too!"
-    }
-    else {
-        return "That's a great choice!"
-    }
-}
-
-function bestSky() {
-    let favAnimal = prompt("What is your favorite flying animal?");
-    if (favAnimal == "eagle"){
-        return "That's mine too!"
-    }
-    else {
-        return "That's a great choice!"
-    }
-}
-
 function ruSure() {
     let firstName = prompt("What's your name?");
     if (firstName != ""){
