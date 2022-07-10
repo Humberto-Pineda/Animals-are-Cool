d4e54339bb635bbb05a7a088c9279ccb1b378913
Humberto Pineda <bpineda7540@gmail.com>
2022-07-09 17:08:02 -0700

    added tryagain

--- app.js
+++ app.js
@@ -8,16 +8,25 @@ function bestBear() {
     }
 }
 
-function ruSure() {
-    let firstName = prompt("What's your name?");
-    if (firstName != ""){
-        return "Nice to meet you!"
+function tryAgain() {
+    let favAnimal = prompt("Guess my favorite land animal? wolf bear tiger");
+    while (favAnimal != "wolf"){
+        answer = prompt("Nope! Try again!");
+        console.log("Nope! Try again!")
+        if (answer == "wolf"){
+            return "WOW! You're great at this!"
+        }
     }
-    else {
-        alert("Well played stranger... refresh page and be honest next time!")
-    }
+    return "WOW! You're great at this!"
 }
 
 function incCount() {
     console.log ("incCount()");
+}
+
+function ratePage(){
+    let starRate = prompt("Rate my page! 1-5");
+    for (let i = 1; i <= starRate; i++){
+        document.write("<img class='wolf-pic' src='wolf.jpg'/>");
+    }
 }
